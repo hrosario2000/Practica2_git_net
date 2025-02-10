a00e672ea52fd102ba5830eb9c9c22839c3c4e2a
hrosario <hrosario@inpe.gob.pe>
2025-02-10 12:39:31 -0500

    ghghj

--- GitNet1/Program.cs
+++ GitNet1/Program.cs
@@ -11,6 +11,7 @@ namespace GitNet1
         static void Main(string[] args)
         {
             //modificado para probar reversion de archivo
+            //modificado en rama_servidor 01
         }
     }
 }
